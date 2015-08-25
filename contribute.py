891f904db22d5c2739650f49566d9d8fdd99cb30
Sea1375 <sea.dream0000@gmail.com>
2015-08-24 20:00:24 -0700

    "Contribution: 2015-08-24 20:00"

--- contribute.py
+++ contribute.py
@@ -93,16 +93,16 @@ def contributions_per_day(args):
 def arguments(argsval):
     parser = argparse.ArgumentParser()
     parser.add_argument('-nw', '--no_weekends',
-                        required=False, action='store_true', default=False,
+                        required=False, action='store_true', default=True,
                         help="""do not commit on weekends""")
-    parser.add_argument('-mc', '--max_commits', type=int, default=7,
+    parser.add_argument('-mc', '--max_commits', type=int, default=5,
                         required=False, help="""Defines the maximum amount of
                         commits a day the script can make. Accepts a number
                         from 1 to 20. If N is specified the script commits
                         from 1 to N times a day. The exact number of commits
                         is defined randomly for each day. The default value
                         is 10.""")
-    parser.add_argument('-fr', '--frequency', type=int, default=60,
+    parser.add_argument('-fr', '--frequency', type=int, default=80,
                         required=False, help="""Percentage of days when the
                         script performs commits. If N is specified, the script
                         will commit N%% of days in a year. The default value
--- history.md
+++ history.md
@@ -520,3 +520,5 @@ Contribution: 2015-12-11 20:01
 
 Contribution: 2015-12-11 20:02
 
+Contribution: 2015-08-24 20:00
+
